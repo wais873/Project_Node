7ce480c6d565234f8bf99ccce83b252231b92a26
wais873 <wais.ghanizada@powercoders.org>
2022-04-14 09:18:20 +0200

    removed the module

---
+++ 13-npm.js
@@ -0,0 +1,28 @@
+// npm  - global command, comes with node
+// npm  --version
+
+// local dependency - can be use only in this particular project
+// npm i <packageName>
+
+// global dependency - can be used in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
+
+// package.json  - manifest file (stores important info about project/package)
+
+    //ways creating package.json
+// manual approach (create package.json in the root, create properties etc)
+// npm init (stet by step, press enter to skip)
+// npm init -y (everything default using -y (y flag))
+
+const _= require ('lodash');
+
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
+
+console.log(items)
+
+console.log("Enjoy the next 24 hours!")
+
+// comand for devdependencies: npm i nodemon -D or inpm i nodemon --save-dev
